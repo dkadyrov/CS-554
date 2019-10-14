225b539f0f889770331be0f30015ee1e5e0c9329
unknown <daniel.kadyrov@gmail.com>
2019-10-14 12:40:15 -0400

    finished hw4

--- Assignments/Assignment_4/src/js/components/App.js
+++ Assignments/Assignment_4/src/js/components/App.js
@@ -1,56 +1,81 @@
 import React from 'react';
 import Editor from './Editor.js';
 import Preview from './Preview';
+const marked = require("marked");
 
 class App extends React.Component {
     constructor(props) {
         super(props);
+        this.downloadMarkdown = this.downloadMarkdown.bind(this);
+        this.downloadHTML = this.downloadHTML.bind(this);
+
         this.state = {
             text: ""
         }
 
-        // this.downloadMarkdown = this.downloadMarkdown.bind(this)
-        // this.downloadHTML = this.downloadHTML.bind(this)
+
     }
 
-
     downloadMarkdown() {
-        const file = new Blob([this.state.text], { type: 'text/plain' });
-
-
+        const elem = document.createElement("a");
+        const file = new Blob([this.state.text], {
+            type: 'text/plain'
+        });
+        elem.href = URL.createObjectURL(file);
+        elem.download = "markdownconvert.md"
+        document.body.appendChild(elem);
+        elem.click();
     }
 
     downloadHTML() {
 
+        const header = '<!DOCTYPE html>\n<html lang="en">\n<head>\n<meta charset="UTF-8">\n<meta name="viewport" content="width=device-width, initial-scale=1.0">\n<meta http-equiv="X-UA-Compatible" content="ie=edge">\n<meta http-equiv="X-UA-Compatible" content="ie=edge">\n<title>Downloaded Markdown</title>\n<style>\ncode {color: red;}\n</style>\n</head>\n<body>\n';
+        const body = marked(this.state.text);
+        const footer = "</body>\n</html>";
+        const html = header + body + footer;
+
+        const elem = document.createElement("a");
+
+        const file = new Blob([html], {
+            type: "text/plain"
+        });
+
+        elem.href = URL.createObjectURL(file);
+        elem.download = "markdownconvert.html"
+        document.body.appendChild(elem);
+        elem.click();
+
     }
 
     updateParentState(markdown) {
-        this.setState({ text: markdown });
+        this.setState({
+            text: markdown
+        });
     }
 
     render() {
         return (
             <div>
-                <div class="row">
-                    <div class="col-6">
-                        <Editor updateParentState={this.updateParentState.bind(this)} />
-
+                <div class="row" >
+                    <div class="col-6" >
+                        <Editor updateParentState={
+                            this.updateParentState.bind(this)
+                        } />
                     </div>
-                    <div class="col-6">
-                        <div class="card">
+                    <div class="col-6" >
+                        <div class="card" >
                             <div class="card-body">
                                 <Preview markdown={this.state.text} />
-
                             </div>
                         </div>
                     </div>
                 </div>
                 <div class="row">
                     <div class="col-6">
-                        <button type="button" class="btn btn-dark btn-block">Download Markdown</button>
+                        <button class="btn btn-dark btn-block" onClick={this.downloadMarkdown}> Download Markdown </button>
                     </div>
                     <div class="col-6">
-                        <button type="button" class="btn btn-dark btn-block">Download HTML</button>
+                        <button class="btn btn-dark btn-block" onClick={this.downloadHTML}> Download HTML </button>
                     </div>
                 </div>
             </div>
@@ -59,86 +84,3 @@ class App extends React.Component {
 }
 
 export default App;
-
-    //     constructor(props) {
-//         super(props)
-//         this.state = {}
-//     }
-
-//     // getText = () => {
-//     //     let newState = this.state;
-//     //     this.setState(converter.makeHtml(newState))
-//     // }
-
-
-//     render() {
-//         return (
-//             <div class="row">
-//                 <div class="col-6">
-//                     <Editor />
-//                 </div>
-//                 <div class="col-6">
-//                     <div class="card">
-//                         <div class="card-body">
-//                             <Preview />
-//                         </div>
-//                     </div>
-//                 </div>
-//             </div>
-//         );
-//     }
-// }
-
-// class Editor extends React.Component {
-//     constructor() {
-//         super();
-//         this.state = {
-//             text: ""
-//         };
-//     };
-
-//     handleChange(event) {
-//         this.setState({ text: event.target.value });
-//         console.log(event.target.value)
-//     };
-
-
-//     render() {
-//         const { editor_text } = this.state;
-//         return (
-//             <form id="editor">
-//                 <div class="form-group">
-//                     <textarea
-//                         type="text"
-//                         class="editor"
-//                         // name="text"
-//                         // value={this.state.text}
-//                         onChange={this.handleChange}
-//                     />
-//                 </div>
-//             </form>
-//         );
-//     }
-// }
-
-// class Preview extends React.Component {
-//     // constructor() {
-//     //     super();
-//     // }
-//     render() {
-//         return (
-//             <div>
-//                 {this.props.text}
-//             </div>
-//         )
-//     }
-// }
-
-// // ReactDOM.render(<App />, document.getElementById('app'));
-
-
-
-
-
-// // ReactDOM.render(<Preview />, document.getElementById('preview'));
-
