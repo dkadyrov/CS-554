006ab49394e6851b0db2ebb4fe87a65f0cd57017
Daniel <Daniel@Daniels-MBP.fios-router.home>
2019-10-28 08:34:43 -0400

    hw5

--- Assignments/Assignment_5/server.js
+++ Assignments/Assignment_5/server.js
@@ -2,25 +2,64 @@ const bluebird = require("bluebird");
 const express = require("express");
 const app = express();
 const redis = require("redis");
+const getById = require("./data/data")
+
+bluebird.promisifyAll(redis);
+
 const client = redis.createClient();
+const visitor = "persons:id:list"
 
-bluebird.promisifyAll(redis.RedisClient.prototype);
-bluebird.promisifyAll(redis.Multi.prototype);
 
-const makeTestPromise = () => {
-  return new Promise((fulfill, reject) => {
-    setTimeout(() => {
-      fulfill({ status: "Good" });
-    }, 4000);
-  });
-};
 
-app.get("/api/people/history", (req, res) => {
-    let makeTestPromiseResult = makeTestPromise();
+app.get("/api/people/history", async(req, res) => {
+  let people;
+  try {
+    people = await client.lrangeAsync(visitor, 0, 19);
+  }
+  catch(error){
+    res.send({status: error.message});
+    return
+  }
 
-    makeTestPromiseResult.then(result => {
-        res.json(result);
-    });
+  let arr = []; 
+
+  for (i=0; i < people.length; i++) {
+    let person = await client.getAsync(people[i]);
+    arr.push(JSON.parse(person));
+  }
+  
+  res.json(arr);
+  return;
+});
+
+app.get("/api/people/:id", async (req, res) => {
+  let id = await client.existsAsync(req.params.id);
+
+  if (id === 1) {
+    let user = await client.getAsync(req.params.id);
+    let person = JSON.parse(user);
+
+    await client.lpushAsync(visitor, req.params.id);
+    res.json(person);
+  } else {
+    let user;
+
+    try {
+      user = await getById(parseInt(req.params.id));
+    } catch (error) {
+      res.send({
+        status: error.message
+      });
+      return
+    }
+
+    let person = JSON.stringify(user);
+
+    await client.setAsync(req.params.id, person);
+    await client.lpushAsync(visitor, req.params.id);
+    res.json(person);
+  }
+  return;
 });
 app.listen(3000, () => {
     console.log("We've now got a server!");
