3a3d82a6aa4bb057d4f7fd872fd9f336410e5c55
Daniel <Daniel@Daniels-MBP.fios-router.home>
2019-11-11 23:32:28 -0500

    hw6

---
+++ Assignments/Assignment_6/server/server.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const redisConnection = require("../redis-connection");
+const worker = require("../worker/worker");
+
+const app = express();
+app.use(bodyParser.json());
+
+app.get("/api/people/:id", async (req, res) => {
+    try {
+        let user = await clientInformation.getAsync(req.params.id);
+        let person = JSON.parse(user);
+
+        res.json(person);
+    } catch (error) {
+        res.send({
+            status: error.message
+        });
+        return
+    }
+})
+
+app.post("/api/people", async (req, res) => {
+    try {
+        let response = await worker.sendMessage({
+            redis: redisConnection,
+            eventName: "add-person",
+            data: {
+                message: req.body.message
+            },
+            expectResponse: false,
+        });
+        res.json({
+            sent: "person added"
+        });
+    } catch (error) {
+        res.send({
+            status: error.message
+        });
+        return
+    }
+});
+
+app.delete("/api/people/:id", async (req, res) => {
+
+});
+
+app.put("/api/people/:id", async( req, res) => {
+
+
+});
+
+app.listen(3000, () => {
+    console.log("We've now got a server!");
+    console.log("Your routes will be running on http://localhost:3000");
+});
---
+++ Assignments/Assignment_6/worker/worker.js
@@ -0,0 +1,4 @@
+const redisConnection = require("./redis-connection");
+const axios = require("axios"); 
+
+const url = "https://gist.githubusercontent.com/philbarresi/5cf15393d245b38a2d86ce8207d5076c/raw/d529fb474c1af347702ca4d7b992256237fa2819/lab5.json"
