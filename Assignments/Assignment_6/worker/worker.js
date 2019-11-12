eb4eeb1c7653e245727175142b2f19cc6e0857b9
Daniel <Daniel@Daniels-MBP.fios-router.home>
2019-11-12 09:38:21 -0500

    worker

--- Assignments/Assignment_6/worker/worker.js
+++ Assignments/Assignment_6/worker/worker.js
@@ -1,4 +1,14 @@
 const redisConnection = require("./redis-connection");
 const axios = require("axios"); 
+const bluebird = require("bluebird");
 
-const url = "https://gist.githubusercontent.com/philbarresi/5cf15393d245b38a2d86ce8207d5076c/raw/d529fb474c1af347702ca4d7b992256237fa2819/lab5.json"
+const url = "https://gist.githubusercontent.com/philbarresi/5cf15393d245b38a2d86ce8207d5076c/raw/d529fb474c1af347702ca4d7b992256237fa2819/lab5.json";
+
+async function main() {
+    redisConnection.on("get-data", async(data, channel) => {
+        const data = await axios.get(url).data;
+
+
+    })
+
+}
