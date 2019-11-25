a60c9d4860eebd437f12ee90c12e559a496d1728
Daniel <Daniel@Daniels-MBP.fios-router.home>
2019-11-25 08:53:00 -0500

    hw7

--- Assignments/Assignment_7/index.js
+++ Assignments/Assignment_7/index.js
@@ -6,19 +6,19 @@ const exphbs = require("express-handlebars");
 
 const Handlebars = require("handlebars");
 
-// const handlebarsInstance = exphbs.create({
-//     defaultLayout: false,
-//     // Specify helpers which are only registered on this instance.
-//     helpers: {
-//         asJSON: (obj, spacing) => {
-//             if (typeof spacing === "number")
-//                 return new Handlebars.SafeString(JSON.stringify(obj, null, spacing));
+const handlebarsInstance = exphbs.create({
+    defaultLayout: false,
+    // Specify helpers which are only registered on this instance.
+    helpers: {
+        asJSON: (obj, spacing) => {
+            if (typeof spacing === "number")
+                return new Handlebars.SafeString(JSON.stringify(obj, null, spacing));
 
-//             return new Handlebars.SafeString(JSON.stringify(obj));
-//         }
-//     },
-//     partialsDir: ["views/partials/"]
-// });
+            return new Handlebars.SafeString(JSON.stringify(obj));
+        }
+    },
+    partialsDir: ["views/partials/"]
+});
 
 app.engine("handlebars", handlebarsInstance.engine);
 app.set("view engine", "handlebars");
